585fde09a0ed3ccb042770c8c4b1e2b3f68e9e55
fuon9 <167889769+fuon9@users.noreply.github.com>
2024-05-11 22:54:29 +0700

    Update account.ts

--- src/api/account.ts
+++ src/api/account.ts
@@ -7,6 +7,7 @@ import {
   AccountNumber,
   AnyNumber,
   AccountName,
+  AccountDate,
   GetAccountCoinsDataResponse,
   GetAccountCollectionsWithOwnedTokenResponse,
   GetAccountOwnedObjectsResponse,
